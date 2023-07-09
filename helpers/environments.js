fe8c45bf1b94a3ab362f3992e1777bcc753694e5
Sanjoy1210 <sanjoypp@gmail.com>
2023-07-09 17:53:55 +0600

    handle check route and make different handler for check

--- helpers/environments.js
+++ helpers/environments.js
@@ -16,6 +16,7 @@ environments.staging = {
     port: 3000,
     envName: 'staging',
     secretKey: 'ashdflajslfasdlfj',
+    maxChecks: 5,
 };
 
 // production environment
@@ -23,6 +24,7 @@ environments.production = {
     port: 5000,
     envName: 'production',
     secretKey: 'owoeijdflsjfoiods',
+    maxChecks: 5,
 };
 
 // determine which environment was passed
